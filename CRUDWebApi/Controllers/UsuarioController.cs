7c3c04bae44aabdc3c7c5dc0d329b68c23b66e8f
leozin777 <leo.cardoso@rede.ulbra.br>
2022-09-26 20:32:28 -0300

    first commit

---
+++ CRUDWebApi/Controllers/UsuarioController.cs
@@ -0,0 +1,76 @@
+﻿using CRUDWebApi.Models;
+using CRUDWebApi.Repository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CRUDWebApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsuarioController : ControllerBase
+    {
+        private readonly IUsuarioRepository _repository;
+        public UsuarioController(IUsuarioRepository repository)
+        {
+            _repository = repository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var usuarios = await _repository.BuscaUsuarios();
+            return usuarios.Any()
+                ? Ok(usuarios)
+                : NoContent();
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var usuario = await _repository.BuscaUsuario(id);
+            return usuario != null
+                ? Ok(usuario)
+                : NotFound("Usuario não encontrado");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post(Usuario usuario)
+        {
+            _repository.AdicionaUsuario(usuario);
+            return await _repository.SaveChangesAsync()
+                ? Ok("usuario add com sucesso")
+                : BadRequest("Erro ao add usuario");
+        }
+
+        [HttpPut("{id:int}")]
+        public async Task<IActionResult> Put(int id, Usuario usuario)
+        {
+            var usuarioBd = await _repository.BuscaUsuario(id);
+            if(usuarioBd == null)
+                NotFound("Usuario não encontrado");
+
+            usuarioBd.Nome = usuario.Nome ?? usuarioBd.Nome;
+            usuarioBd.DataNascimento = usuario.DataNascimento != new DateTime()
+                ? usuario.DataNascimento : usuarioBd.DataNascimento;
+
+            _repository.AtualizaUsuario(usuarioBd);
+
+            return await _repository.SaveChangesAsync()
+                ? Ok("usuario att com sucesso")
+                : BadRequest("Erro ao att usuario");
+        }
+
+        [HttpDelete("{id:int}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var usuarioDel = await _repository.BuscaUsuario(id);
+            if(usuarioDel == null)
+                NotFound("Usuario não encontrado");
+
+            _repository.DeletaUsuario(usuarioDel);
+
+            return await _repository.SaveChangesAsync()
+                ? Ok("usuario del com sucesso")
+                : BadRequest("Erro ao del usuario");
+        }
+    }
+}
---
+++ CRUDWebApi/Data/UsuarioContext.cs
@@ -0,0 +1,26 @@
+﻿using CRUDWebApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CRUDWebApi.Data
+{
+    public class UsuarioContext : DbContext
+    {
+        public UsuarioContext(DbContextOptions<UsuarioContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Usuario> Usuarios { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            var usr = modelBuilder.Entity<Usuario>();
+            usr.ToTable("tb_usuario");
+            usr.HasKey(x => x.Id);
+            usr.Property(x => x.Id).HasColumnName("id").ValueGeneratedOnAdd();
+            usr.Property(x => x.Nome).HasColumnName("nome").IsRequired();
+            usr.Property(x => x.DataNascimento).HasColumnName("data_nascimento");
+
+
+        }
+    }
+}
